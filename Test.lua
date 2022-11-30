4c28ef232d24d68e06658ab04ded8327485f977f
MuhXd <momo2010sofia@gmail.com>
2022-11-30 22:11:37 +0000

    Update

--- Test.lua
+++ Test.lua
@@ -1,11 +1,12 @@
-print("Loaded")
+
 local GamesLoader = {
     [{6839171747}] = "",
 }
 
 for ids, url in next, GamesLoader do
     if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))(); return true
+        loadstring(game:HttpGet(url))(); 
+        return true
     end
 end
 
