235db6dba00cdc7318ef2ee4cd8c596e9fedf81c
MuhXd <momo2010sofia@gmail.com>
2022-12-01 23:44:40 +0000

    tt

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,13 @@
 -- Loads the Game 
 local GamesLoader = {
-    [{6839171747}] = "",
+    [{6839171747}] = "PlaceIDGame",
+    [{6516141723}] = "PlaceIDLobby",
 }
 -- Loops and Returns True if Works
 for ids, url in next, GamesLoader do
+    print("Finding")
     if table.find(ids, game.PlaceId) then
+        print("found")
         loadstring(game:HttpGet(url))(); 
         return true
     end
